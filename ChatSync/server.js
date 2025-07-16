3457b38f38307b9f164d6e2a95481bbebe684337
KAKADIYA VAIRAG <vairag.synfinity@gmail.com>
2025-07-16 22:20:20 +0530

    add basic socket.io

---
+++ ChatSync/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
