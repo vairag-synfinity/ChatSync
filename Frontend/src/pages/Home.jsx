6d4f0958d9a2b28e2d894c3ad7473644eed34811
KAKADIYA VAIRAG <vairag.synfinity@gmail.com>
2025-07-21 14:21:26 +0530

    add

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -9,4 +9,5 @@ export default function HomePage(){
         <ChatApp/>
         </>
     )
-}                                                                                                                                
+}             
+
